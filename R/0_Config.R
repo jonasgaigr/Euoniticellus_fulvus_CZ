e9bad24e5775ff0c3cba6ecbe8448c99d1885a12
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-08-02 21:24:46 +0200

    init config script

---
+++ R/0_Config.R
@@ -0,0 +1,8 @@
+#--- instalace (pokud chybí) ---------------------
+
+#--- načtení knihoven -----------------------------
+library(pdftools)   # pdftools::pdf_text
+library(stringr)    # stringr::str_detect, stringr::str_extract, stringr::str_trim, stringr::str_replace_all
+library(dplyr)      # dplyr::tibble, dplyr::mutate, dplyr::case_when, dplyr::select, dplyr::rowwise, dplyr::ungroup, dplyr::filter, dplyr::arrange
+library(tidyr)      # tidyr::separate
+library(rndop)
