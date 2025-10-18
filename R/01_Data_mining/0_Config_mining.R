34f134bcbf844aabfdfc8dc2e9478ab24db540b1
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-10-18 22:54:33 +0200

    add reference data

--- R/01_Data_mining/0_Config_mining.R
+++ R/01_Data_mining/0_Config_mining.R
@@ -1,16 +1,54 @@
 #----------------------------------------------------------#
-# Load packages -----
+# Install and load packages -----
 #----------------------------------------------------------#
-if(!isTRUE(require(tidyverse, quietly = TRUE))) {
-  install.packages("tidyverse", dependencies = TRUE); library(tidyverse)
-} else {
-  require(tidyverse)
+packages <- c("tidyverse", "pdftools", "RCzechia")
+
+for (pkg in packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+  library(pkg, character.only = TRUE)
 }
 
-if(!isTRUE(require(pdftools, quietly = TRUE))) {
-  install.packages("pdftools", dependencies = TRUE); library(pdftools)
-} else {
-  require(pdftools)
-}
+#----------------------------------------------------------#
+# Load reference data -----
+#----------------------------------------------------------#
+# Borders of Czechia
+czechia_border <- 
+  RCzechia::republika(
+    resolution = "high"
+  ) %>%
+  sf::st_transform(
+    ., 
+    st_crs("+init=epsg:5514")
+  ) 
 
+# Data on protected areas and mapping fields
+endpoint <- "http://gis.nature.cz/arcgis/services/Aplikace/Opendata/MapServer/WFSServer?"
+caps_url <- base::paste0(endpoint, "request=GetCapabilities&service=WFS")
 
+layer_name_sitmap1rad <- "Opendata:Mapovaci_sit_-_deleni_1.radu"
+
+getfeature_url_sitmap1rad <- paste0(
+  endpoint,
+  "service=WFS&version=2.0.0&request=GetFeature&typeName=", layer_name_sitmap1rad
+)
+
+sitmap <- sf::st_read(getfeature_url_sitmap1rad) %>%
+  sf::st_transform(
+    ., 
+    st_crs("+init=epsg:5514")
+  ) %>%
+  sf::st_crop(
+    .,
+    czechia_border  # crop by the border of Czechia
+  )
+
+#----------------------------------------------------------#
+# Load occurrence data -----
+#----------------------------------------------------------#
+data_ndop <- 
+  readr::read_csv2(
+    "Data/Input/data_ndop.csv",
+    locale = locale(encoding = "Windows-1250")
+  )
